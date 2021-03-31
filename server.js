f64adb1b3eb72bdb8d9f4bc0395cfdc6f39ab450
bensvarnum <bens.varnum@gmail.com>
2021-03-31 18:59:01 -0400

    boilerplat + mongo connection

---
+++ server.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+//routes below cors
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Jungle");
+});
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
+
+mongoose.connection.on("connected", () => {
+  console.log("Mongoose is connected");
+});
+
+mongoose.set("useFindAndModify", false);
